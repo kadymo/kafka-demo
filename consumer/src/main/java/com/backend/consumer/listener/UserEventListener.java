ce952ef81516da90e1d50e92a9bdfaea2213a2ac
Kádymo <santanakadymo@gmail.com>
2025-07-27 22:31:01 -0300

    FEAT: Principal Kafka listener for User Events, verifying the event type

---
+++ consumer/src/main/java/com/backend/consumer/listener/UserEventListener.java
@@ -0,0 +1,89 @@
+package com.backend.consumer.listener;
+
+import com.backend.consumer.model.UserEvent;
+import com.backend.consumer.service.EventProcessingService;
+import com.backend.consumer.service.UserEventProcessingService;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.internals.events.EventProcessor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.support.Acknowledgment;
+import org.springframework.kafka.support.KafkaHeaders;
+import org.springframework.messaging.handler.annotation.Header;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserEventListener {
+    private static final Logger logger = LoggerFactory.getLogger(UserEventListener.class);
+
+    private final UserEventProcessingService userEventProcessingService;
+    private final EventProcessingService eventProcessingService;
+
+    public UserEventListener(UserEventProcessingService userEventProcessingService, EventProcessingService eventProcessingService) {
+        this.userEventProcessingService = userEventProcessingService;
+        this.eventProcessingService = eventProcessingService;
+    }
+
+    // Principal Listener
+    @KafkaListener(
+            topics = "user-events",
+            groupId = "event-processing-group-user-events",
+            containerFactory = "userEventKafkaListenerContainerFactory"
+    )
+    public void handleUserEvent(@Payload UserEvent userEvent,
+                                @Header(KafkaHeaders.RECEIVED_TOPIC) String topic,
+                                @Header(KafkaHeaders.RECEIVED_PARTITION) String partition,
+                                @Header(KafkaHeaders.OFFSET) long offset,
+                                ConsumerRecord<String, UserEvent> record,
+                                Acknowledgment acknowledgment
+                                ) {
+        logger.info("Received user event: topic={}, partition={}, offset={}, key={}, event={}", topic, partition, offset, record.key(), userEvent);
+        try {
+            if (eventProcessingService.isEventAlreadyProcessed(userEvent.getEventId())) {
+                logger.info("Event {} already processed, ignoring.", userEvent.getEventId());
+                acknowledgment.acknowledge();
+                return;
+            }
+
+            switch (userEvent.getEventType()) {
+                case "LOGIN":
+                    userEventProcessingService.processLoginEvent(userEvent);
+                    break;
+                case "LOGOUT":
+                    userEventProcessingService.processLogoutEvent(userEvent);
+                    break;
+                case "REGISTER":
+                    userEventProcessingService.processRegisterEvent(userEvent);
+                    break;
+                default:
+                    userEventProcessingService.processGenericUserEvent(userEvent);
+            }
+
+            eventProcessingService.markEventAsProcessed(
+                    userEvent.getEventId(),
+                    userEvent.getEventType(),
+                    topic,
+                    partition,
+                    offset
+            );
+
+            acknowledgment.acknowledge();
+            logger.info("User Event {} processed successfully", userEvent.getEventId());
+        } catch (Exception e) {
+            logger.error("Error while processing user event {}: {}", userEvent.getEventId(), e.getMessage());
+
+            eventProcessingService.markEventAsError(
+                    userEvent.getEventId(),
+                    userEvent.getEventType(),
+                    topic,
+                    partition,
+                    offset,
+                    e.getMessage()
+            );
+
+            throw e;
+        }
+    }
+}
